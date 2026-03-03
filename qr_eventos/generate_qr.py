12eb987651721031383804b9cc4f8e61dee697bb
Alejandro <alejandropulido2809@gmail.com>
2026-03-04 00:36:36 +0100

    nuevo commit

--- qr_eventos/generate_qr.py
+++ qr_eventos/generate_qr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-url = "lively-zuccutto-4e6bf7.netlify.app"
+url = "lively-zuccutto-4e6bf7.netlify.app/prueba.pdf"
 
 qr = qrcode.QRCode(
     version = 1,
@@ -11,6 +11,6 @@ qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save("qr_code.png")
+img.save("qr_prueba.png")
 
-print("QR code generated and saved as qr_code.png")
+print("QR code generated and saved as qr_prueba.png")
