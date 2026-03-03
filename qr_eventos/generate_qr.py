dbbbc265bb281a81b736cfc9ff6b08eb810aaaa8
Alejandro <alejandropulido2809@gmail.com>
2026-03-04 00:32:10 +0100

    Primeras pruebas

---
+++ qr_eventos/generate_qr.py
@@ -0,0 +1,16 @@
+import qrcode
+
+url = "lively-zuccutto-4e6bf7.netlify.app"
+
+qr = qrcode.QRCode(
+    version = 1,
+    box_size=10,
+    border=4
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("qr_code.png")
+
+print("QR code generated and saved as qr_code.png")
