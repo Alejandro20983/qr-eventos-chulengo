98805566b4fd7456a86145ccbb1b02a18d2a5c49
Alejandro <alejandropulido2809@gmail.com>
2026-03-05 11:04:37 +0100

    Se crea nuevo qr para la presentación del flyer del evento del sabado

--- generate_qr.py
+++ generate_qr.py
@@ -11,6 +11,6 @@ qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save("Flyer_Chulengo.jpg")
+img.save("Flyer_Chulengo2.jpg")
 
-print("QR code generated and saved as Flyer_Chulengo.jpg")
+print("QR code generated and saved as Flyer_Chulengo2.jpg")
