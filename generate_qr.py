3e02688ffc82e506b2f2e1481ba8e2adfacba563
Alejandro <alejandropulido2809@gmail.com>
2026-03-05 23:37:01 +0100

    Ultimo cambio para el qr final

--- generate_qr.py
+++ generate_qr.py
@@ -1,6 +1,6 @@
 import qrcode
 
-url = "https://lively-zuccutto-4e6bf7.netlify.app/Flyer_Chulengo.jpg"
+url = "https://lively-zuccutto-4e6bf7.netlify.app/chulengo_final.jpg"
 
 qr = qrcode.QRCode(
     version=1,
@@ -12,6 +12,6 @@ qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save("Flyer_Chulengo_QR.jpg")
+img.save("Flyer_Chulengo_QR_final.jpg")
 
-print("QR code generated and saved as Flyer_Chulengo_QR.jpg")
+print("QR code generated and saved as Flyer_Chulengo_QR_final.jpg")
