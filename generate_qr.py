d62eda4b6530dec61cc0a9e9964d878d6acc51f8
Alejandro <alejandropulido2809@gmail.com>
2026-03-05 11:28:16 +0100

    Se crea nuevo qr y se genera, para probar con el fleyer original

--- generate_qr.py
+++ generate_qr.py
@@ -1,16 +1,17 @@
 import qrcode
 
-url = "lively-zuccutto-4e6bf7.netlify.app/Flyer_Chulengo.jpg"
+url = "https://lively-zuccutto-4e6bf7.netlify.app/Flyer_Chulengo.jpg"
 
 qr = qrcode.QRCode(
-    version = 1,
+    version=1,
     box_size=10,
     border=4
 )
+
 qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
-img.save("Flyer_Chulengo.jpg")
+img.save("Flyer_Chulengo_QR.jpg")
 
-print("QR code generated and saved as Flyer_Chulengo.jpg")
+print("QR code generated and saved as Flyer_Chulengo_QR.jpg")
